881c2f463e5804120022ccc03df83def8817e69e
IanCarter <iancarter.work@gmail.com>
2022-08-02 20:40:11 +0000

    Fix : Dependencies

--- index.php
+++ index.php
@@ -29,11 +29,10 @@ require_once __DIR__ . "/doton/autoload.php";
  */
 
 
- echo "<h1>DotonCli Terminal</h1> ";
+echo "<h1>DotonCli Terminal</h1> ";
 
- echo "<div>Name : " . Terminal::name . "</div> ";
+echo "<div>Name : " . Terminal::name . "</div> ";
 
- echo "<div>Version : " . Terminal::version . "</div> ";
+echo "<div>Version : " . Terminal::version . "</div> ";
 
- echo "<div>Serie : " . Terminal::serie . "</div> ";
-    
+echo "<div>Serie : " . Terminal::serie . "</div> ";
