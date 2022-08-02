5cfb97bd39b7fc94f0e0fdb53f43614ff6ac95d6
IanCarter <iancarter.work@gmail.com>
2022-08-02 20:34:19 +0000

    Fix : Exemple

--- index.php
+++ index.php
@@ -29,12 +29,11 @@ require_once __DIR__ . "/doton/autoload.php";
  */
 
 
- var_dump( 
+ echo "<h1>DotonCli Terminal</h1> ";
+
+ echo "<div>Name : " . Terminal::name . "</div> ";
+
+ echo "<div>Version : " . Terminal::version . "</div> ";
+
+ echo "<div>Serie : " . Terminal::serie . "</div> ";
     
-    Terminal::name, 
-    
-    Terminal::version, 
-    
-    Terminal::serie 
-    
-);
